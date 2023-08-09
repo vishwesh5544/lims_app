7835ddeaff364e36a618621bde876a9d681c9437
Vishwesh Shukla <vishweshshukla20@gmail.com>
2023-08-09 15:41:47 +0530

    patient view tests

--- lib/screens/patient_management.dart
+++ lib/screens/patient_management.dart
@@ -1,13 +1,20 @@
+import 'package:barcode/barcode.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lims_app/bloc/in_transit_bloc/in_transit_bloc.dart';
+import 'package:lims_app/bloc/in_transit_bloc/in_transit_event.dart';
+import 'package:lims_app/bloc/in_transit_bloc/in_transit_state.dart';
 import 'package:lims_app/bloc/patient_bloc/patient_bloc.dart';
 import 'package:lims_app/bloc/patient_bloc/patient_event.dart';
 import 'package:lims_app/bloc/patient_bloc/patient_state.dart';
 import 'package:lims_app/components/lims_table.dart';
 import 'package:lims_app/components/buttons/redirect_button.dart';
 import 'package:lims_app/components/search_header.dart';
+import 'package:lims_app/models/patient.dart';
 import 'package:lims_app/test_items/redirect_to_test_menu.dart';
 import 'package:lims_app/test_items/test_data.dart';
+import 'package:lims_app/utils/barcode_utility.dart';
 import 'package:lims_app/utils/strings/button_strings.dart';
 import 'package:lims_app/utils/strings/route_strings.dart';
 import 'package:lims_app/utils/strings/search_header_strings.dart';
@@ -101,43 +108,75 @@ class _PatientManagementState extends State<PatientManagement> {
   }
 
   /// invoice dialog
-  Future<void> _showPreviewDialog(value) async {
+  Future<void> _showPreviewDialog(Patient patient) async {
+    BlocProvider.of<InTransitBloc>(context).add(SearchPatient(patient.emailId));
+    List<String> columns = ["#", "Test Name", "Sample Type", "Test Code", "Cost", "Tax%", "Price Total", "Barcode"];
+    print(patient.emailId);
     return showDialog(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-            insetPadding: EdgeInsets.zero,
-            titleTextStyle: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20.0),
-            titlePadding: EdgeInsets.zero,
-            title: Container(
-              padding: const EdgeInsets.all(10.0),
-              decoration: BoxDecoration(color: ColorProvider.blueDarkShade),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const SizedBox(),
-                  const Text('Test Details'),
-                  InkWell(
-                    child: const Icon(Icons.cancel_rounded, color: Colors.white),
-                    onTap: () => Navigator.pop(context, "Cancel"),
-                  )
-                ],
-              ),
-            ),
-            content: Container(
-              // height: 600,
-              width: 600,
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 1),
-                  borderRadius: const BorderRadius.all(Radius.circular(5))),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [],
-                ),
-              ),
-            ));
+        return BlocConsumer<InTransitBloc, InTransitState>(
+            builder: (context, state) {
+              return AlertDialog(
+                  insetPadding: EdgeInsets.zero,
+                  titleTextStyle: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20.0),
+                  titlePadding: EdgeInsets.zero,
+                  title: Container(
+                    padding: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(color: ColorProvider.blueDarkShade),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(),
+                        const Text('Test Details'),
+                        InkWell(
+                          child: const Icon(Icons.cancel_rounded, color: Colors.white),
+                          onTap: () => Navigator.pop(context, "Cancel"),
+                        )
+                      ],
+                    ),
+                  ),
+                  content: Container(
+                    height: 600,
+                    width: 1080,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, width: 1),
+                        borderRadius: const BorderRadius.all(Radius.circular(5))),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          DataTable(
+                              dataRowHeight: 85,
+                              headingRowColor: MaterialStateProperty.all(Colors.black),
+                              headingTextStyle: const TextStyle(color: Colors.white),
+                              dataRowColor: MaterialStateProperty.all(Colors.grey.shade300),
+                              columns: columns.map((el) => DataColumn(label: Text(el))).toList(),
+                              rows: state.testsList!.map((test) {
+                                return DataRow(cells: [
+                                  DataCell(Text("${test.id}")),
+                                  DataCell(Text(test.testName)),
+                                  DataCell(Text(test.sampleType)),
+                                  DataCell(Text(test.testCode)),
+                                  DataCell(Text("${test.price}")),
+                                  DataCell(Text("${test.taxPercentage}")),
+                                  DataCell(Text("${test.totalPrice}")),
+                                  DataCell(
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: SvgPicture.string(BarcodeUtility.getBarcodeSvgString(
+                                          "{patientId: ${patient.id}, testId: ${test.id}")),
+                                    ),
+                                  ),
+                                ]);
+                              }).toList())
+                        ],
+                      ),
+                    ),
+                  ));
+            },
+            listener: (context, state) {});
       },
     );
   }
